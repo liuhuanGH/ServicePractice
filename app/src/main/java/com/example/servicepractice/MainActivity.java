f4374679f0e448719a27e657a61464d78c76524d
liuhuan01 <liuhuan01@coocaa.com>
2018-10-25 20:53:18 +0800

    注2
    Change-Id: I0ebc6c7212ee3bcd01e974e026f7138bb732bfeb

--- app/src/main/java/com/example/servicepractice/MainActivity.java
+++ app/src/main/java/com/example/servicepractice/MainActivity.java
@@ -57,12 +57,12 @@ public class MainActivity extends Activity implements View.OnClickListener{
     }
 
     private void initView() {
-//        startButton = new Button(this);
-//        startButton.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-//        startButton.setText("开始下载");
-//        startButton.setId(R.id.startDownload);
-//        linearLayout.addView(startButton);
-
+        startButton = new Button(this);
+        startButton.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        startButton.setText("开始下载");
+        startButton.setId(R.id.startDownload);
+        linearLayout.addView(startButton);
+//
 //        pauseButton = new Button(this);
 //        pauseButton.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
 //        pauseButton.setText("暂停下载");
