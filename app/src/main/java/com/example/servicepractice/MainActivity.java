6f0d9aa098ca76db9f7a190fec77ba4f848b0d4f
liuhuan01 <liuhuan01@coocaa.com>
2018-10-25 20:55:24 +0800

    注12
    Change-Id: Id5610fe8dd62ce5a8be5b0c410ed7914448845d0

--- app/src/main/java/com/example/servicepractice/MainActivity.java
+++ app/src/main/java/com/example/servicepractice/MainActivity.java
@@ -57,17 +57,17 @@ public class MainActivity extends Activity implements View.OnClickListener{
     }
 
     private void initView() {
-        startButton = new Button(this);
-        startButton.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        startButton.setText("开始下载");
-        startButton.setId(R.id.startDownload);
-        linearLayout.addView(startButton);
-
-        pauseButton = new Button(this);
-        pauseButton.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        pauseButton.setText("暂停下载");
-        pauseButton.setId(R.id.pauseDownload);
-        linearLayout.addView(pauseButton);
+//        startButton = new Button(this);
+//        startButton.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+//        startButton.setText("开始下载");
+//        startButton.setId(R.id.startDownload);
+//        linearLayout.addView(startButton);
+//
+//        pauseButton = new Button(this);
+//        pauseButton.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+//        pauseButton.setText("暂停下载");
+//        pauseButton.setId(R.id.pauseDownload);
+//        linearLayout.addView(pauseButton);
 
         cancelButton = new Button(this);
         cancelButton.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
